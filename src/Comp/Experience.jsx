0d15e44fe743865e49f9d7deffc0620981e40879
shaan Yadav <shaanyworkspace@gmail.com>
2025-06-26 02:54:16 +0530

    Added mediplan data

--- src/Comp/Experience.jsx
+++ src/Comp/Experience.jsx
@@ -30,7 +30,7 @@ const Experience = () => {
                                     <div className="absolute inset-0 bg-gradient-to-t from-black/60 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
                                 </div>
                             </div>
-                            {/* Main Content */}
+                            {/* Main Content*/}
                             <div className="relative w-full lg:w-2/3 max-w-2xl bg-neutral-900 p-6 rounded-2xl shadow-lg border border-neutral-800 transition-all duration-500 group-hover:-translate-y-2 group-hover:shadow-xl group-hover:border-purple-700">
                                 <h3 className="text-xl font-bold text-white mb-2">
                                     Project: <span className="text-green-400">{project.title}</span>
